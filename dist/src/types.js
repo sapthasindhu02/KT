8a90d97e135c9ce1981f88ace612ea6aafea3657
sapthasindhu02 <sapthasindhu.v11@gmail.com>
2022-04-29 15:06:28 +0530

    changes commited

--- dist/src/types.js
+++ dist/src/types.js
@@ -4,6 +4,5 @@ exports.TYPES = void 0;
 const TYPES = {
     User_Service: Symbol.for("User_Service"),
     User_controller: Symbol.for("User_controller"),
-    UserDetails: Symbol.for("UserDetails"),
 };
 exports.TYPES = TYPES;
